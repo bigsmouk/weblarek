56de1b26bcb05333a5b785d43c4eec7d09987f63
bigsmouk <timursemeneev004@gmail.com>
2026-04-12 13:24:21 +0300

    fix: удалены неиспользуемые переменные

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import './scss/styles.scss';
 import { Api } from './components/base/Api';
 import { LarekApi } from './components/LarekApi';
 import { ProductsModel } from './components/models/ProductsModel';
-import { CartModel } from './components/models/CartModel';
-import { BuyerModel } from './components/models/BuyerModel';
 import { API_URL } from './utils/constants';
 
 const productsModel = new ProductsModel();
-const cartModel = new CartModel();
-const buyerModel = new BuyerModel();
 const api = new Api(API_URL);
 const larekApi = new LarekApi(api);
 
